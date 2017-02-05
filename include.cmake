55ba96ec8730994bd8b7b3ae42cc44324a9590a1
Justin Berger <jberger@melco.com>
2017-02-05 13:04:32 -0700

    Turn off tool compilation

--- include.cmake
+++ include.cmake
@@ -3,6 +3,6 @@ if(MSVC)
     CGET_HAS_DEPENDENCY(glew NUGET_PACKAGE glew.${CGET_MSVC_RUNTIME} GITHUB nigels-com/glew VERSION glew-1.11.0 NUGET_USE_STATIC)
 else()
     CGET_HAS_DEPENDENCY(glew URL https://glew.s3.amazonaws.com/nigels-com/glew/15/15.1/glew-1.13.0.zip
-                             CMAKE_PATH glew-1.13.0/build/cmake )
+                             CMAKE_PATH build/cmake OPTIONS -DBUILD_UTILS:BOOL=off )
 endif()
 

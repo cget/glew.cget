c68ddfe55440d81a01c741294fa9148642ed23c0
Justin <j.david.berger@gmail.com>
2017-02-12 00:46:18 -0700

    Changed to new nuget packge

--- include.cmake
+++ include.cmake
@@ -1,6 +1,7 @@
 if(MSVC)
-    ADD_DEFINITIONS(-DGLEW_STATIC=1)
-    CGET_HAS_DEPENDENCY(glew NUGET_PACKAGE glew.${CGET_MSVC_RUNTIME} GITHUB nigels-com/glew VERSION glew-1.11.0 NUGET_USE_STATIC)
+	# This includes glew
+	CGET_HAS_DEPENDENCY(GLUT NUGET_PACKAGE nupengl.core GITHUB dcnieho/FreeGLUT VERSION FG_3_0_0)
+	find_package(glew)
 else()
     CGET_HAS_DEPENDENCY(glew URL https://glew.s3.amazonaws.com/nigels-com/glew/15/15.1/glew-1.13.0.zip
                              CMAKE_PATH build/cmake OPTIONS -DBUILD_UTILS:BOOL=off )
